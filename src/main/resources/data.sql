0c76ff4baea711313e4c18ff66903e7ddae1ef3c
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2024-06-15 11:33:02 +0200

    Comments added.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,7 @@ USE exam_db;
 
 -- INSERT INTO categories (name, created, edited) VALUES ('Vegetar',NOW(),NOW());
 
+    -- Dummy data
 INSERT INTO test (name) VALUES ('Bob');
 INSERT INTO test (name) VALUES ('Lisa');
 INSERT INTO test (name) VALUES ('Preben');

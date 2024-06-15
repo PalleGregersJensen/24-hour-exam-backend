722bd69d6788f0ba0a5f0bdeaa15f0273cb09b4d
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2024-06-15 11:03:22 +0200

    Folders created. Connection to database created.

---
+++ src/main/java/org/example/finalexambackend/FinalExamBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.finalexambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalExamBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/finalexambackend/FinalExamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.finalexambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalExamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
